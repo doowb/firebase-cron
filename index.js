4a6eda1c54b2cff829c5a3da1febc8b67ec5f5e2
Brian Woodward <brian.woodward@gmail.com>
2015-12-31 16:51:25 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * firebase-cron <https://github.com/doowb/firebase-cron>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
